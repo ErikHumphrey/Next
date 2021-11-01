dde8fa6b920738f0c20d8018802e7fc8b34b6437
Carlos Cao <carlos.cao2@gmail.com>
2021-11-01 16:00:57 +1300

    Added content to Home

---
+++ pages/main.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <div>
+      <h1>Hello there, welcome to this page.</h1>
+      <h2>I am currently learning many things and excited about the future.</h2>
+      <h3>
+        I have a strong passion for new technologies. Therefore, I want to be
+        always updated on everything that is exciting in the news regarding Tech
+        such as AI, ML, IoT, VR and AR. I hope that my enthusiasm for technology
+        will make me more motivated for the future.
+      </h3>
+    </div>
+  );
+}
